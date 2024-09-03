eb00665ac808f3ec28bbd385c6a49646e60a768a
kyoul10121 <kyoul10121@naver.com>
2024-09-04 00:44:55 +0900

    Design: 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import TodoTemplete from './components/TodoTemplete';
+
+const appStyle = css`
+  background: radial-gradient(#e3fafc, #99e9f2, #22b8cf);
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 function App() {
   return (
-    <div
-      css={{
-        background: 'blue',
-      }}
-    >
-      test commit
+    <div css={appStyle}>
+      <TodoTemplete />
     </div>
   );
 }
