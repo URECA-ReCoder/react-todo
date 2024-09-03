85abe444baf9c17eb6039a231fe7376cad7bec0b
hyorish03 <hyorish2001@gmail.com>
2024-09-03 22:56:58 +0900

    Feat:TodoList 렌더링 기능 구현

--- src/Components/TodoList.jsx
+++ src/Components/TodoList.jsx
@@ -1,20 +1,55 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
-function TodoList() {
+function TodoList({ todoList }) {
   return (
     <div
       css={css({
         height: '235px',
-        width: '100%',
+        width: '324px',
         borderBottom: 'solid 1px lightgray',
         padding: '18px 18px 0 18px',
       })}
     >
-      <div css={css({ fontSize: '18px' })}>📋 To Do (n)</div>
+      <div css={css({ fontSize: '18px', marginBottom: '15px' })}>
+        📋 To Do ({todoList.length})
+      </div>
       <div
-        css={css({ width: '100%', height: '156px', overflowY: 'scroll' })}
-      ></div>
+        css={css({
+          width: '100%',
+          height: '156px',
+          overflowY: 'scroll',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '10px',
+        })}
+      >
+        {todoList.map((todo, index) => (
+          <div
+            key={index}
+            css={css({
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              height: '20px',
+              width: '80%',
+            })}
+          >
+            <div
+              css={css({
+                display: 'flex',
+                flexDirection: 'row',
+                textAlign: 'center',
+                alignItems: 'center',
+                width: '100%',
+              })}
+            >
+              *<div>{todo.todo}</div>
+              <div>❌</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
