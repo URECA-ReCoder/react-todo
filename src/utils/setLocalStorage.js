af732c8d63d9d26f49f6303e55d3df6a104feac5
hyorish03 <hyorish2001@gmail.com>
2024-09-03 22:55:18 +0900

    Fix: setLocalStorage 함수 내의 id 지정 방식 수정

--- src/utils/setLocalStorage.js
+++ src/utils/setLocalStorage.js
@@ -1,8 +1,9 @@
 import { getLocalStorage } from './getLocalStorage';
 
 export const setLocalStorage = (value) => {
-  const newTodo = getLocalStorage().concat({
-    id: localStorage.getItem('todo').length + 1,
+  const prevTodo = getLocalStorage();
+  const newTodo = prevTodo.concat({
+    id: prevTodo.length + 1,
     todo: value,
     done: false,
   });
