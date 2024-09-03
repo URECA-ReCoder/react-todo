c856ea83a109d017a1f19b86fbe05e4c6d247251
kyoul10121 <kyoul10121@naver.com>
2024-09-04 00:43:18 +0900

    Feat(Design): map을 이용한 출력, 삭제기능, 완료 체크 기능 추가

--- src/components/TodoItemList.jsx
+++ src/components/TodoItemList.jsx
@@ -2,39 +2,71 @@
 import React from 'react';
 import { css } from '@emotion/react';
 import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
-const todoItemList = css`
-  padding-left: 20px;
-`;
 const todoItemWrapper = css`
-  padding: 10px;
+  display: inline-block;
+  height: 40%;
+  padding: 0 20px 0;
 `;
-const todoCount = css`
-  padding: 10px;
+const todoItemList = css`
+  margin-top: 40px;
+  padding-left: 20px;
+  height: 60%;
+  overflow: auto;
 `;
-const todoItemCount = css`
-  margin: 20px;
+const todoItemTitle = css`
+  margin: 10px;
 `;
-const hrStyle = css`
-  background-color: steelblue;
+const line = css`
+  margin-bottom: 10px;
+  color: lightgray;
 `;
-const TodoItemList = () => {
+
+const TodoItemList = ({ title, todoList, setTodoList, checkedList }) => {
   return (
     <section className="todo-item-list" css={todoItemWrapper}>
-      <div css={todoCount}>
-        <hr css={hrStyle} />
-        <span className="todo-item-title" css={todoItemCount}>
-          0개의 할 일이 남았습니다.
+      <div>
+        <hr css={line} />
+        <span className="todo-item-title" css={todoItemTitle}>
+          {title}
         </span>
-        <hr css={hrStyle} />
+        <hr css={line} />
       </div>
       <ul css={todoItemList}>
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {todoList && // todoList가 있을 때만 출력
+          todoList.map((todoItem) => {
+            // map을 이용하여 TodoItem을 출력
+            // checkedList 값에 따라 '할 일 목록' 또는 '완료한 목록'을 출력
+            if (checkedList != todoItem.checked) return null;
+
+            // 삭제한 항목인 경우, 출력하지 않음 (deleted가 true)
+            if (todoItem.deleted) return null;
+
+            return (
+              <TodoItem
+                key={todoItem.id}
+                todoItem={todoItem}
+                todoList={todoList}
+                setTodoList={setTodoList}
+              />
+            );
+          })}
       </ul>
     </section>
   );
+};
+
+TodoItemList.propTypes = {
+  title: PropTypes.string.isRequired,
+  todoList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ),
+  setTodoList: PropTypes.func.isRequired,
+  checkedList: PropTypes.bool.isRequired,
 };
 
 export default TodoItemList;
