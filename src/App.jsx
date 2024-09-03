c93b636ef5c81a9576ae7b96b4db155a4fbd4389
hyorish03 <hyorish2001@gmail.com>
2024-09-03 21:16:04 +0900

    Feat: Header, TodoList, DoneList 컴포넌트 App.jsx에 import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import TodoList from './Components/TodoList';
+import DoneList from './Components/DoneList';
+import Header from './Components/Header';
+
 function App() {
-  return <div>React Todo</div>;
+  return (
+    <div
+      css={css({
+        width: '100vw',
+        height: '100vh',
+        color: 'black',
+        backgroundColor: 'red',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      })}
+    >
+      <div
+        css={css({
+          width: '360px',
+          height: '600px',
+          backgroundColor: 'white',
+          borderRadius: '20px',
+          boxShadow: '0px 0px 25px rgba(0, 0, 0, 0.25)',
+        })}
+      >
+        <Header />
+        <TodoList />
+        <DoneList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
